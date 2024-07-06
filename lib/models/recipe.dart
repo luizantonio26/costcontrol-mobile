a967b039a06e436e386639466e785f07ad3bceab
Luiz Antonio Rozendo <luiz.rozendo01@gmail.com>
2024-07-06 14:24:09 -0300

    feat: created recipe_detail class

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,3 +1,28 @@
+import 'package:mobile/models/ingredient.dart';
+
+class RecipeIngredient {
+  final int id;
+  final double quantity;
+  final double value;
+  final Ingredient ingredient;
+
+  RecipeIngredient({
+    required this.id,
+    required this.quantity,
+    required this.value,
+    required this.ingredient,
+  });
+
+  factory RecipeIngredient.fromJson(Map<String, dynamic> json) {
+    return RecipeIngredient(
+      id: json['id'],
+      quantity: json['quantity'].toDouble(),
+      value: json['value'].toDouble(),
+      ingredient: Ingredient.fromJson(json['ingredient']),
+    );
+  }
+}
+
 class Recipe {
   final int id;
   final String name;
@@ -7,6 +32,7 @@ class Recipe {
   final double recipeCost;
   final double recipeCostPerUnit;
   final DateTime createdAt;
+  final List<dynamic>? ingredients;
 
   Recipe({
     required this.id,
@@ -17,6 +43,7 @@ class Recipe {
     required this.recipeCost,
     required this.recipeCostPerUnit,
     required this.createdAt,
+    this.ingredients,
   });
 
   factory Recipe.fromJson(Map<String, dynamic> json) {
@@ -28,6 +55,7 @@ class Recipe {
       imageUrl: json['image_url'],
       recipeCost: json['recipe_cost'].toDouble(),
       recipeCostPerUnit: json['recipe_cost_per_unit'].toDouble(),
+      ingredients: json['ingredients'],
       createdAt: DateTime.parse(json['created_at']),
     );
   }
